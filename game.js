398eea0e7491e5cbe68bf7fdeb9df6da11c0e914
graf-jordan <jordangraf42@gmail.com>
2023-10-06 19:44:28 +1030

    Begin implementing change from opacity to transform

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let compChoice = Math.floor(Math.random()*3);
     let computerIcon;
@@ -8,42 +11,59 @@ function getComputerChoice(){
     } else {
         computerIcon = "compPaper";
     }
-    document.getElementById(computerIcon).setAttribute("style", "opacity:0.5");
+    document.getElementById(computerIcon).style.transform = "scale(1.5, 1.5)";
     return compChoice;
 }
 
 function playGame(player, computer, input) {
-    console.log("test2");
-    console.log(player);
-    console.log(computer);
+    //console.log("test2");
+    //console.log(player);
+    //console.log(computer);
     let victor;
-    let score = player - computer;
+    let score = player + computer;
     if (player - computer == 0) {
-        victor = "Draw!";
-    } else if (player + computer == 2) {
-        victor = "Paper wins!";
-    } else if (player + computer == 3) {
-        victor = "Scissors Wins!";
-    } else if (player + computer == 1) {
-        victor = "Rock wins!";
+        victor = "DRAW";
+    } else if (score == 2) {
+        victor = "PAPER WINS";
+    } else if (score == 3) {
+        victor = "SCISSORS WINS";
+    } else if (score == 1) {
+        victor = "ROCK WINS";
     }
+    //console.log(score);
 
     const showResult = document.querySelector("#result");
     result.textContent = victor;
-    counter(player, computer, input);
+    counter(score, input);
 }
 
 function getPlayerChoice(input, num) {
 
-    document.getElementById(input).setAttribute("style", "opacity:0.5");
+    document.getElementById(input).style.transform = "scale(1.5, 1.5)";
     let compResult = getComputerChoice();
-    console.log("test");
+    removeTransform(input, compResult);
+   // console.log("test");
     playGame(num, compResult, input);
 } 
 
-function removeOpacity(player, computer) {
+    function removeTransform(player, computer) {
+        document.getElementById(player).style.transform = "initial";
+    }
 
-}
-
-function counter(player, computer, input) {
+function counter(score, input) {
+    let result;
+    if (score == 0) {
+        playerScore = playerScore;
+        computerScore = computerScore;
+    }
+    else if ((score == 2 && input == "paper") || (score == 3 && input == "scissors") || (score == 1 && input == "rock")) {
+        playerScore++;
+        result = document.querySelector("#playerScore");
+        result.textContent = playerScore;
+    } else {
+        computerScore++;
+        result = document.querySelector("#computerScore");
+        result.textContent = computerScore;
+    }
+    
 }
