f3d4dcd08da9d6464ddc3fbab69dea5dca64b2c4
graf-jordan <jordangraf42@gmail.com>
2023-10-06 16:58:34 +1030

    Fix incorrect play being shown due to wrong number input

--- game.js
+++ game.js
@@ -4,9 +4,9 @@ function getComputerChoice(){
     if (compChoice == 0) {
         computerIcon = "compRock";
     } else if (compChoice == 1) {
-        computerIcon = "compPaper";
-    } else {
         computerIcon = "compScissors";
+    } else {
+        computerIcon = "compPaper";
     }
     document.getElementById(computerIcon).setAttribute("style", "opacity:0.5");
     return compChoice;
@@ -16,18 +16,22 @@ function playGame(player, computer) {
     console.log("test2");
     console.log(player);
     console.log(computer);
+    let victor;
     if (player - computer == 0) {
-        alert("Draw!");
-    } else if (player - computer == 1) {
-        alert("Paper wins!");
-    } else if (player - computer == 2 || player - computer == -2) {
-        alert("Rock wins!");
-    } else if (player - computer == -1) {
-        alert("Scissors wins!");
+        victor = "Draw!";
+    } else if (player + computer == 2) {
+        victor = "Paper wins!";
+    } else if (player + computer == 3) {
+        victor = "Scissors Wins!";
+    } else if (player + computer == 1) {
+        victor = "Rock wins!";
     }
+    const showResult = document.querySelector("#result");
+    result.textContent = victor;
 }
 
 function getPlayerChoice(input, num) {
+    
     document.getElementById(input).setAttribute("style", "opacity:0.5");
     let compResult = getComputerChoice();
     console.log("test");
