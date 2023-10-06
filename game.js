8670f08ec2dd7be9b05ebf11bbb30300fb76b7c1
graf-jordan <jordangraf42@gmail.com>
2023-10-06 15:35:28 +1030

    Implement basic game function

--- game.js
+++ game.js
@@ -1,15 +1,35 @@
 function getComputerChoice(){
-
+    let compChoice = Math.floor(Math.random()*3);
+    let computerIcon;
+    if (compChoice == 0) {
+        computerIcon = "compRock";
+    } else if (compChoice == 1) {
+        computerIcon = "compPaper";
+    } else {
+        computerIcon = "compScissors";
+    }
+    document.getElementById(computerIcon).setAttribute("style", "opacity:0.5");
+    return compChoice;
 }
 
-function getPlayerChoice(){
-
+function playGame(player, computer) {
+    console.log("test2");
+    console.log(player);
+    console.log(computer);
+    if (player - computer == 0) {
+        alert("Draw!");
+    } else if (player - computer == 1) {
+        alert("Paper wins!");
+    } else if (player - computer == 2 || player - computer == -2) {
+        alert("Rock wins!");
+    } else if (player - computer == -1) {
+        alert("Scissors wins!");
+    }
 }
 
-function playGame() {
-
-}
-
-function choose(input) {
+function getPlayerChoice(input, num) {
     document.getElementById(input).setAttribute("style", "opacity:0.5");
-}
+    let compResult = getComputerChoice();
+    console.log("test");
+    playGame(num, compResult);
+} 
